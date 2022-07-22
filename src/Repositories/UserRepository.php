1b4ae477add38c4080aac4daf7b1c1f6a49be136
patrick <patrickbahatiduss.pbd@gmail.com>
2022-07-22 19:12:39 +0200

    user repos

---
+++ src/Repositories/UserRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Referenzverwaltung\Repositories;
+
+use Referenzverwaltung\Models\User;
+
+/**
+ * Class TypeOfWorkRepository
+ * @package Referenzverwaltung\Repositories
+ * @version December 19, 2020, 3:49 pm UTC
+*/
+
+class TypeOfWorkRepository extends BaseRepository
+{
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
+    ];
+
+    /**
+     * Return searchable fields
+     *
+     * @return array
+     */
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
+
+    /**
+     * Configure the Model
+     **/
+    public function model()
+    {
+        return User::class;
+    }
+
+    public function getByEmail($email){
+        return User::where('email', $email)->first();
+    }
+    
+}
